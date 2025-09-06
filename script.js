91b744c01a16549a60efda921c997c76fc5638d2
howardshowered <194877133+howardshowered@users.noreply.github.com>
2025-09-05 22:46:24 -0700

    added button click eventlistener with barebones dialog

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 const myLibrary = [];
-
+const dialog = document.getElementById('myDialog');
+const closeButton = document.getElementById('closeDialog'); 
 function Book(title, author, genre) {
     this.title = title;
     this.author = author;
@@ -20,6 +21,7 @@ function displayLibrary() {
     const cardContainer = document.querySelector(".card-container");
     myLibrary.forEach( (book) => {
         const newBookDiv = document.createElement("div"); 
+        newBookDiv.classList.add("card");
         const newBookTitle = document.createElement("h3");
         const newBookAuthor = document.createElement("p");
         const newBookGenre = document.createElement("p");
@@ -51,3 +53,13 @@ addBookToLibrary("The Lord of the Rings, Fellowship of the Ring", "J.R.R. Tolkie
 addBookToLibrary("The Lord of the Rings, Fellowship of the Ring", "J.R.R. Tolkien", "fantasy");
 
 displayLibrary();
+
+const button = document.querySelector(".button-add-book");
+button.addEventListener("click", (event)=> {
+    dialog.showModal();
+});
+
+
+    closeButton.addEventListener('click', () => {
+        dialog.close();
+    });
